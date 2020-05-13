64a5b1b57fb25f3e4fa3bc7a86364e8511c160f2
FabriceG7 <goret.goret@sfr.fr>
2020-05-13 16:26:11 +0200

    jqueryex2

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#text').show();  //Version show
+    //$('#text').css('display', 'block');  Version CSS
+    //$('text').toggle(); //Version toggle
+});
+
+/*Plusieurs propriétés CSS:
+$(document).ready(function(){
+    $('#text').css({
+        'display':'block',
+        'color':'red'
+    });
+}); */
